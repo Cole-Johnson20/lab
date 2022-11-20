2360b2fcd9d5efca6b547210eb61910aa10d3e59
Cole-Johnson20 <118510051+Cole-Johnson20@users.noreply.github.com>
2022-11-20 17:22:30 -0600

    added account.py

--- account.py
+++ account.py
@@ -1,35 +1,20 @@
 class Account:
-    def __init__(self, name: str) -> None:
-        """
-        Constructs all necessary values for the Account class
-        :param name: The name that is associated with the account
-        """
-        self.__account_name = str(name)
+    def __init__(self, name):
+        self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount: float) -> bool:
-        """
-        Allows the user to deposit money to the account i.e adding money to the account
-        :param amount: Amount of money that the user wants deposited in their account
-        :return:True if transaction is successful and False if transaction does not happen due to invalidity
-        """
+    def deposit(self, amount):
         try:
             if amount <= 0:
                 return False
             else:
                 self.__account_balance += amount
                 return True
-        except TypeError:
-            print('Could not compare str to int')
+        except ValueError:
+            print('Invalid input.')
             raise
 
-    def withdraw(self, amount: float) -> bool:
-
-        """
-        Allows the user to withdraw money from the account i.e subtracting money from the account
-        :param amount: Amount of money that
-        :return:True if transaction is successful and False if transaction does not happen due to invalidity
-        """
+    def withdraw(self, amount):
         try:
             if amount <= 0:
                 return False
@@ -38,22 +23,13 @@ class Account:
             else:
                 self.__account_balance -= amount
                 return True
-        except TypeError:
-            print('Could not compare str to int')
+        except ValueError:
+            print('Invalid input.')
             raise
 
-    def get_balance(self) -> float:
+        def get_balance(self):
+            return self.__account_balance
 
-        """
-        Gets the total balance of the account
-        :return:Total balance of the account
-        """
-        return self.__account_balance
-
-    def get_name(self) -> str:
-
-        """
-        Get the name associated with the account
-        :return: Name of the account
-        """
-        return self.__account_name
+        def get_name(self):
+            return self.__account_name
+        
